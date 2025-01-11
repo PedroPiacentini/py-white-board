a54300d0d58c41fb5c4262afaad0404aa3fe5357
Pedro Piacentini <pedropiacentini@icloud.com>
2025-01-11 16:40:39 +0000

    list all instances

--- oop/index.py
+++ oop/index.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
@@ -7,6 +8,8 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price * self.quantity
@@ -16,10 +19,11 @@ class Item:
 
 item1 = Item("Phone", 100, 5)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-item1.apply_discount()
-
-item2.pay_rate = 0.7
-item2.apply_discount()
+for instance in Item.all:
+    print(instance.name)
 
 
